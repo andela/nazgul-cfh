ba790667c530a0236fd370b1b57d169efb4ca63f
Gabriel Micah <gabrielsuave17@gmail.com>
2018-04-04 12:40:44 +0100

    chore(landing-page): integrated ui template
    - Replace index html body with new UI
    - Link materialize and animate.css
    - Modified layout to fit mobile views
    
    [Finishes: 156100324]

--- public/js/init.js
+++ public/js/init.js
@@ -5,11 +5,19 @@
 // window.init = function() {
 //     window.bootstrap();
 // };
-if (window.location.hash == "#_=_") window.location.hash = "#!";
+if (window.location.hash === '#_=_') window.location.hash = '#!';
 // $(document).ready(function() {
 //     //Fixing facebook bug with redirect
-    
+
 
 //     //Then init the app
 //     window.init();
 // });
+
+(function ($) {
+  $(() => {
+    $('.sidenav').sidenav();
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+  }); // end of document ready
+}(jQuery)); // end of jQuery name space
