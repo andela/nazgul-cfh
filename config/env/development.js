0138b8ea367d8f4a58cb017406c68c6c8583a232
Felglitzzz <andeladeveloper@Andelas-MacBook-Pro-44.local>
2018-04-06 16:06:38 +0100

    feature(signup): implement social media signup fix heroku errors update .env-example file

--- config/env/development.js
+++ config/env/development.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 
 export default {
   port: 3001,
---
+++ config/env/production.js
@@ -0,0 +1,31 @@
+export default {
+  port: 3001,
+  app: {
+    name: 'Cards for Humanity'
+  },
+
+  twitter: {
+    clientID: process.env.TWITTER_CONSUMER_KEY_PROD,
+    clientSecret: process.env.TWITTER_CONSUMER_SECRET,
+    callbackURL: process.env.TWITTER_CALLBACK_URL_PROD
+  },
+
+  facebook: {
+    clientID: process.env.FB_CLIENT_ID_PROD,
+    clientSecret: process.env.FB_CLIENT_SECRET_PROD,
+    callbackURL: process.env.FB_CALLBACK_URL_PROD
+
+  },
+
+  google: {
+    clientID: process.env.GOOGLE_CLIENT_ID_PROD,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET_PROD,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL_PROD
+  },
+
+  github: {
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.GITHUB_CALLBACK_URL_PROD
+  },
+};
