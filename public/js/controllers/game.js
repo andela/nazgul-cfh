4db3745da722f6e94bcce79c557b2d40390d8303
Efosaok <efosaokpugie@gmail.com>
2018-04-09 11:52:02 +0100

    remove formerly implemented method of invite

--- public/js/controllers/game.js
+++ public/js/controllers/game.js
@@ -223,15 +223,6 @@ angular.module('mean.system')
           // Once the game ID is set, update the URL if this is a game with friends,
           // where the link is meant to be shared.
           $location.search({game: game.gameID});
-          if(!$scope.modalShown){
-            setTimeout(function(){
-              var link = document.URL;
-              var txt = 'Give the following link to your friends so they can join your game: ';
-              $('#lobby-how-to-play').text(txt);
-              $('#oh-el').css({'text-align': 'center', 'font-size':'22px', 'background': 'white', 'color': 'black'}).text(link);
-            }, 200);
-            $scope.modalShown = true;
-          }
         }
       }
     });
