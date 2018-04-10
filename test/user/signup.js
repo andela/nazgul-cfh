93441f2dcbd9c92cdb37694ae0f792fedc7c5946
Efosaok <efosaokpugie@gmail.com>
2018-04-10 23:05:44 +0100

    add more user-based tests

--- test/user/signup.js
+++ test/user/signup.js
@@ -1,10 +1,10 @@
-
 import supertest from 'supertest';
 import { expect } from 'chai';
 import mongoose from 'mongoose';
 import app from '../../dist/server';
-
 const request = supertest.agent(app);
+console.log(process.env.NODE_ENV);
+console.log()
 
 // mongoose.connection.db.dropDatabase();
 after((done) => {
@@ -114,6 +114,35 @@ describe('Signup API', () => {
         expect(err).to.be.null;
         expect(res.body).to.be.an('object');
         done();
+      });
+  });
+});
+
+describe('POST /api/search/users', () => {
+  const searchQuery = {
+    emailOrUsernameOfFriend: 'testtt@test.com',
+  };
+  it('performs a retrieval if email is passed', (done) => {
+    request
+      .post('/api/search/users')
+      .set('Content-Type', 'application/json')
+      .send(searchQuery)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        console.log(res.body), 'jdhJHSGSGGSG'
+        expect(res.body.user.email).to.equal('efosaokpugie@gmail.com')
+      });
+  });
+  it('performs a retrieval if username is sent', (done) => {
+    searchQuery.emailOrUsernameOfFriend = 'Full name';
+    request
+      .post('/api/search/users')
+      .set('Content-Type', 'application/json')
+      .send(searchQuery)
+      .expect(200, done)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.user.name).to.equal('full name');
       });
   });
 });
