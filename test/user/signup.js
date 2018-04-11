1a239e6ee021f17958236d38deb373ae3924e609
Efosaok <efosaokpugie@gmail.com>
2018-04-11 08:13:31 +0100

    add more tests

--- test/user/signup.js
+++ test/user/signup.js
@@ -4,8 +4,8 @@ import mongoose from 'mongoose';
 import app from '../../dist/server';
 const request = supertest.agent(app);
 console.log(process.env.NODE_ENV);
-console.log()
 
+let token;
 // mongoose.connection.db.dropDatabase();
 after((done) => {
   mongoose.connection.db.dropDatabase(done);
@@ -110,57 +110,63 @@ describe('Signup API', () => {
         password: 'password'
       })
       .end((err, res) => {
-        console.log(res.body);
         expect(res.status).to.equal(201);
         expect(res.body).to.be.an('object');
-        done();
-      });
-  });
-  it('performs a retrieval if email is passed', (done) => {
-    const searchQuery = {
-      emailOrUsernameOfFriend: 'tester@test.com',
-    };
-    request
-      .post('/api/search/users')
-      .set('Content-Type', 'application/json')
-      .send(searchQuery)
-      .end((err, res) => {
-        expect(res.status).to.equal(200);
-        console.log(res.body), 'jdhJHSGSGGSG'
-        expect(res.body.user.email).to.equal('efosaokpugie@gmail.com')
+        token = res.body.token;
         done();
       });
   });
 });
 
 describe('POST /api/search/users', () => {
-  it('performs a retrieval if email is passed', (done) => {
+  const searchQuery = {
+    emailOrUsernameOfFriend: 'tester@test.com',
+  };
+  it('returns an error message if user is not authenticated', (done) => {
     request
       .post('/api/search/users')
       .set('Content-Type', 'application/json')
       .send(searchQuery)
       .end((err, res) => {
-        expect(res.status).to.equal(200);
-        console.log(res.body), 'jdhJHSGSGGSG'
-        expect(res.body.user.email).to.equal('efosaokpugie@gmail.com')
-        done();
-      });
-  });
-  it('performs a retrieval if username is sent', (done) => {
-    const searchQuery = {
-      emailOrUsernameOfFriend: 'Full name',
-    };
-    request
-      .post('/api/search/users')
-      .set('Content-Type', 'application/json')
-      .send(searchQuery)
-      .end((err, res) => {
-        expect(res.status).to.equal(200);
-        expect(res.body.user.name).to.equal('full name');
+        expect(res.status).to.equal(403);
+        expect(res.body.error).to.equal('You have to login First')
         done();
       });
   });
 });
+describe('POST /api/invite/users', () => {
+  const searchQuery = {
+    emailOrUsernameOfFriend: 'tester@test.com',
+    link: 'localhost:3000/#!/app?game=83747mdbbf',
+  };
+  it('returns an error message if user is not authenticated', (done) => {
+    request
+      .post('/api/invite/users')
+      .set('Content-Type', 'application/json')
+      .send(searchQuery)
+      .end((err, res) => {
+        expect(res.status).to.equal(403);
+        expect(res.body.error).to.equal('You have to login First');
+        done();
+      });
+  });
+});
+// it('performs a retrieval if username is sent', (done) => {
+//   const searchQuery = {
+//     emailOrUsernameOfFriend: 'user_test',
+//   };
+//   request
+//     .post('/api/search/users')
+//     .set('Content-Type', 'application/json')
+//     .set('authorization', token)
+//     .send(searchQuery)
+//     .end((err, res) => {
+//       expect(res.status).to.equal(200);
+//       expect(res.body.message).to.equal('full name');
+//       done();
+//     });
+// });
+
 describe('Signin', () => {
   it('should not allow a user to login with no email', (done) => {
     request
@@ -239,4 +245,3 @@ describe('Signin', () => {
       });
   });
 });
-
