1fa446e868e1513c146b6c83244dc38ff28a8300
TAIWO KOLAWOLE <kolawole.taiwo@andela.com>
2018-04-12 07:56:07 +0100

    fix(remove-conflict-tag): removing git conflict tag

--- public/js/init.js
+++ public/js/init.js
@@ -9,11 +9,6 @@ if (window.location.hash === '#_=_') window.location.hash = '#!';
 // $(document).ready(function() {
 //     //Fixing facebook bug with redirect
 
-
 //     //Then init the app
 //     window.init();
 // });
-<<<<<<< HEAD
-
-=======
->>>>>>> db52d4b4ce0e1750421f52f8871325ad341b93f9
