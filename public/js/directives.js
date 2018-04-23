b119b8f90d8f96ee560e7c58d76004f5640e1593
Efosaok <efosaokpugie@gmail.com>
2018-04-23 11:49:55 +0100

    fix hound violations

--- public/js/directives.js
+++ public/js/directives.js
@@ -45,7 +45,7 @@ angular.module('mean.directives', [])
                 }
               }
             } else {
-              curQ.text += ' '+startStyle+scope.game.table[scope.game.winningCard].card[0].text + endStyle;
+              curQ.text += ` ${startStyle+scope.game.table[scope.game.winningCard].card[0].text + endStyle}`;
             }
           }
         });
